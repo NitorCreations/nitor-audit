560dee14fa5eaa58d99a8658c20d536b1a301739
Lasse Lilja <lasse.lilja@iki.fi>
2021-05-28 10:09:09 +0300

    shellchecked the audit.sh and made args real args

--- bin/audit.sh
+++ bin/audit.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 __SELF="${0##*/}"
 set -euxo pipefail
-echo "$__SELF"
+
+ARG_USER="${1:?USAGE $__SELF [USER_ID] [TARGET_HOST] [TARGET_GPG_PUBLIC_KEY]]}"
+ARG_HOST="${2:?USAGE $__SELF [USER_ID] [TARGET_HOST] [TARGET_GPG_PUBLIC_KEY]]}"
+ARG_KEY="${3:?USAGE $__SELF [USER_ID] [TARGET_HOST] [TARGET_GPG_PUBLIC_KEY]]}"
 
 ARG_UUID=$(cat /proc/sys/kernel/random/uuid)
 ARG_ISO_DATE="$(date --iso-8601)"
@@ -12,17 +15,15 @@ ARG_HASH=$(sudo md5sum "$ARG_SOURCE")
 utmpdump /var/run/utmp  > latest_utmp.log
 utmpdump /var/log/wtmp > latest_wtmp.log
 
-#needs sudo
 utmpdump /var/log/btmp > latest_btmp.log
 uptime > latest_uptime.log
 uname -a > latest_unamea.log
 ARG_HASH2=$(cat latest_utmp.log latest_wtmp.log latest_btmp.log latest_uptime.log latest_unamea.log | md5sum)
 echo "$ARG_ISO_DATE $ARG_UUID $ARG_HASH $ARG_HASH2" > checksum.file
 
-#neds sudo 
 tar -czvf "$ARG_FILENAME" "$ARG_SOURCE" latest_utmp.log latest_wtmp.log latest_btmp.log latest_uptime.log latest_unamea.log checksum.file
-time gpg --no-tty --always-trust --encrypt --recipient $3 "$ARG_FILENAME"
-sftp -i /root/.ssh/nitor-audit -o StrictHostKeyChecking=accept-new $1@$2 <<EOF
+time gpg --no-tty --always-trust --encrypt --recipient "$ARG_KEY" "$ARG_FILENAME"
+sftp -i /root/.ssh/nitor-audit -o StrictHostKeyChecking=accept-new "$ARG_USER"@$"$ARG_HOST"<<EOF
 put "$ARG_FILENAME".gpg
 bye
 EOF
